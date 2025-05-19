2d7e3b1f1fdfa8a5bff4d0586ea883e14be19e9f
knu9910 <knu9910@naver.com>
2025-05-19 15:00:30 +0900

    css: font-color css 리펙토링

--- src/components/ui/tiptap/menus/font-color.tsx
+++ src/components/ui/tiptap/menus/font-color.tsx
@@ -1,10 +1,11 @@
-import { AiOutlineFontColors } from 'react-icons/ai';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { useEditorContext } from '../context/editor-context';
 import { useFontConfigStore } from '../plugin';
 import { Colors } from '../plugin/tiptap-font-config/constants';
 import { cn } from '@/lib/utils';
-import { MdFormatColorText } from 'react-icons/md';
+import { Pipette } from 'lucide-react';
+import { IconButtonWrapper } from './common/icon-button-wrapper';
+import { IconButton } from './common/icon-button';
 
 type Props = React.HTMLAttributes<HTMLElement>;
 
@@ -29,16 +30,20 @@ export const TipTapFontColor = ({ className }: Readonly<Props>) => {
     <>
       {/* 색상 선택 */}
       <Popover>
-        <PopoverTrigger className={cn('hover:bg-gray-200 p-1  cursor-pointer px-2 flex items-center', className)}>
-          <AiOutlineFontColors style={{ color: selectedColor }} className='size-6' />
+        <PopoverTrigger className={cn('', className)}>
+          <IconButtonWrapper>
+            <IconButton>
+              <Pipette style={{ color: selectedColor }} className="" />
+            </IconButton>
+          </IconButtonWrapper>
         </PopoverTrigger>
-        <PopoverContent className='flex flex-wrap justify-around flex-col h-[160px] w-[200px] bg-slate-100 rounded-md'>
+        <PopoverContent className="flex flex-wrap justify-around flex-col h-[160px] w-[200px] bg-slate-100 rounded-md">
           {Object.values(Colors).map((color, idx) => (
             <div
               key={color + idx}
               onClick={() => changeTextColor(color)}
               style={{ backgroundColor: color }}
-              className='w-[16px] h-[16px] rounded-sm cursor-pointer'
+              className="w-[16px] h-[16px] rounded-sm cursor-pointer"
             ></div>
           ))}
         </PopoverContent>
