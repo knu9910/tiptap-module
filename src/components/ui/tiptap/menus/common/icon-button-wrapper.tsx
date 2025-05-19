9b9551bca924750f675bb1ea3d8370a0c5b5f16c
knu9910 <knu9910@naver.com>
2025-05-19 14:31:24 +0900

    feat: 공통 css 모듈화

---
+++ src/components/ui/tiptap/menus/common/icon-button-wrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+type IconButtonWrapperProps = React.HTMLAttributes<HTMLDivElement> & {
+  children: React.ReactNode;
+};
+
+export const IconButtonWrapper = ({ children, className, ...props }: Readonly<IconButtonWrapperProps>) => (
+  <div
+    className={cn(
+      'h-full flex gap-2 items-center rounded-lg p-2 justify-center transition-all hover:bg-gray-100',
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </div>
+);
---
+++ src/components/ui/tiptap/menus/common/icon-button.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+export const IconButton = ({ children, className, ...props }: Readonly<IconButtonProps>) => {
+  return (
+    <button
+      type="button"
+      className={cn('text-gray-500 size-5 flex hover:text-gray-900 transition-colors', className)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
