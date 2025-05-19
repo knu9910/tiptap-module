3b735fcd08660ad36c63a0cf6393e703f18b1d65
knu9910 <knu9910@naver.com>
2025-05-19 18:11:07 +0900

    feat: active css 구성

--- src/components/ui/tiptap/menus/common/icon-button.tsx
+++ src/components/ui/tiptap/menus/common/icon-button.tsx
@@ -8,7 +8,11 @@ type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 export const IconButton = ({ children, className, ...props }: Readonly<IconButtonProps>) => {
   return (
     <span
-      className={cn('text-gray-500 size-4 flex items-center hover:text-gray-900 transition-colors', className)}
+      className={cn(
+        'text-gray-500 size-4 flex items-center hover:text-gray-900 transition-colors',
+        '[&[data-state=on]]:text-blue-600',
+        className
+      )}
       {...props}
     >
       {children}
