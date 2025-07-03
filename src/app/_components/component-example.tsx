c23c58f6863c8db36096b08f20e1133db156b424
knu9910 <knu9910@naver.com>
2025-07-03 16:03:19 +0900

    css: toolbar 모바일 반응형 1차 구현

--- src/app/_components/component-example.tsx
+++ src/app/_components/component-example.tsx
@@ -6,10 +6,10 @@ type Props = React.HTMLAttributes<HTMLElement>;
 
 export const ComponentExample = ({ className }: Readonly<Props>) => {
   return (
-    <div className={cn('', className)}>
+    <div className={cn('w-300 p-2', className)}>
       <TiptapEditor keyId={'12'} />
       <h2 className="mb-3 mt-6 text-xl font-semibold">너는 viewer야 </h2>
-      <TiptapViewer className="border p-4" keyId={'12'} />
+      <TiptapViewer className="borderㅁㅁㅁㅁ p-4" keyId={'12'} />
 
       <TiptapEditor keyId={'123'} />
       <h2 className="mb-3 mt-6 text-xl font-semibold">너는 viewer야 </h2>
